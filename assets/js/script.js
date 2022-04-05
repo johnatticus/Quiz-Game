ab9915d3f5a6e438d33d098f88a4b4e160819c15
John Hysong <johnahysong@gmail.com>
2022-04-04 23:17:57 -0400

    added timer function to js and displayed on HTML now

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@
 var quizBegin = document.getElementById("quizarea");
 var questionEl = document.getElementById("question")
 var choicesEl = document.getElementById("answers");
-var timeEl = document.getElementById("time");
+var timerArea = document.getElementById("timeleft");
 var initialsEl = document.getElementById("initials");
 var startBtn = document.getElementById("start");
 var submitBtn = document.getElementById("submit");
@@ -18,7 +18,18 @@ var scoreScreen = document.getElementById("score-screen");
 var answerChoices = document.getElementById("answers")
 var answerButtons = document.getElementById("answer-button")
 
-let shuffledQuestions, currentQuestionIndex
+// let shuffledQuestions
+
+// variable to make sure question index starts at 0 so the quiz begins with the first question in array
+var currentQuestionIndex = 0
+//creating a let to define later as the current question as the user goes through them
+let currentQuestion
+// timer variable
+// timer countdown variable
+var timerCount
+
+
+
 // questions for the quiz
 var questions = [
     {
@@ -49,17 +60,19 @@ startBtn.addEventListener('click', beginQuiz);
 
 //function to start the quiz and bring up questions
 function beginQuiz() {
+    startTimer()
     console.log("Started")
     startScreen.classList.add('hide')
     quizBegin.classList.remove('hide')
-    shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
+    currentQuestion = questions[currentQuestionIndex]
     getQuestions()
+    
+
 }
 
 //function to get the questions
 function getQuestions() {
-    showQuestion(shuffledQuestions[currentQuestionIndex])
+    showQuestion(currentQuestion)
         // document.write ( question );
         // var options = questions[i].choices;
         // document.body.appendChild(document.createElement("br"));
@@ -68,7 +81,26 @@ function getQuestions() {
 // puts the random qauestion picked on the page
 function showQuestion(question) {
     questionEl.innerText = question.question
+    // questions.forEach(choices) 
+    console.log(answer)
+        
+    }
+
+// function to start a timer count    
+function startTimer() {
+    // Sets timer
+    var timeLeft = 10;
+    setInterval(function() {
+    timeLeft--;
+    if (timeLeft >= 0) {
+    timerArea.textContent = timeLeft;
+    }
+        }, 1000);
 }
+    // for each for question.choices
+    // make a button for each choice
+    // give each button a value of correct answer
+
     
 
 // function to create the score and push it to high score
